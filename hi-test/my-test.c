60e4a7b0c622c38052440aa46371aefb22d85a16
zzrubyonly <3310591527@qq.com>
2025-02-17 10:00:56 +0800

    添加代码my-test代码

---
+++ hi-test/my-test.c
@@ -0,0 +1,20 @@
+//
+// Created by rubyonlyz on 2025/2/17.
+//
+//
+// Created by rubyonlyz on 2025/2/15.
+#include <stdio.h>
+
+int main() {
+
+  int s[]={1,2,3,4,5,6,7,8,9};
+
+  for(int i=0;i<9;i++) {
+
+    printf("%d",s[i]);
+    printf("\n");
+
+  }
+
+    return 0;
+}
