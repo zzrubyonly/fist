dcb9e98a0a7520c185b9faafb09c3dedf52a6fbd
zzrubyonly <3310591527@qq.com>
2025-02-17 09:51:12 +0800

    添加代码

---
+++ three.c
@@ -0,0 +1,20 @@
+//
+// Created by rubyonlyz on 2025/2/17.
+//
+
+//
+// Created by rubyonlyz on 2025/2/16.
+//
+//
+// Created by rubyonlyz on 2025/2/16.
+//
+
+#include <stdio.h>
+
+int main() {
+
+    printf("22222222");
+
+    return 0;
+
+}
